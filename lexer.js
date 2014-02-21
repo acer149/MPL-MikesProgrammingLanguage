2266c18106f6a314cf1a33f034c656d3b7bff19e
Mike Hunter <randomemail149@gmail.com>
2014-02-20 20:47:31 -0500

    Lexing edits, trying to finetune the use of a matrix

--- lexer.js
+++ lexer.js
@@ -16,8 +16,11 @@
     
     var lineNumber = 1;;
     
-    var openQuote = false;
+    var unrecognizedSymbol = false;
     var endOfFileReached = false;
+    var openQuote = false;
+    var openParen = false;
+    var whiteSpaceCount = 0; //Keeps track of extra spaces and skips over them
     
     function test() {
     	document.getElementById("taOutput").value = "";
@@ -37,7 +40,7 @@
         			  [{"a":8, "b":8, "c":8, "d":8, "e":8, "f":8, "g":8, "h":8, "i":5, "j":8, "k":8, "l":8, "m":8, "n":8, "o":8, "p":8, "q":8, "r":8, "s":8, "t":8, "u":8, "v":8, "w":8, "x":8, "y":8, "z":8, "0":8, "1":8, "2":8, "3":8, "4":8, "5":8, "6":8, "7":8, "8":8, "9":8}],
         			  [{"a":8, "b":8, "c":8, "d":8, "e":8, "f":8, "g":8, "h":8, "i":8, "j":8, "k":8, "l":8, "m":8, "n":6, "o":8, "p":8, "q":8, "r":8, "s":8, "t":8, "u":8, "v":8, "w":8, "x":8, "y":8, "z":8, "0":8, "1":8, "2":8, "3":8, "4":8, "5":8, "6":8, "7":8, "8":8, "9":8}],
         			  [{"a":8, "b":8, "c":8, "d":8, "e":8, "f":8, "g":8, "h":8, "i":8, "j":8, "k":8, "l":8, "m":8, "n":8, "o":8, "p":8, "q":8, "r":8, "s":8, "t":7, "u":8, "v":8, "w":8, "x":8, "y":8, "z":8, "0":8, "1":8, "2":8, "3":8, "4":8, "5":8, "6":8, "7":8, "8":8, "9":8}],
-        			  [{"a":8, "b":8, "c":8, "d":8, "e":8, "f":8, "g":8, "h":8, "i":8, "j":8, "k":8, "l":8, "m":8, "n":8, "o":8, "p":8, "q":8, "r":8, "s":8, "t":8, "u":8, "v":8, "w":8, "x":8, "y":8, "z":8, "0":8, "1":8, "2":8, "3":8, "4":8, "5":8, "6":8, "7":8, "8":8, "9":8, " ":'T_PRINT', "\n":'T_PRINT'}],
+        			  [{"a":8, "b":8, "c":8, "d":8, "e":8, "f":8, "g":8, "h":8, "i":8, "j":8, "k":8, "l":8, "m":8, "n":8, "o":8, "p":8, "q":8, "r":8, "s":8, "t":8, "u":8, "v":8, "w":8, "x":8, "y":8, "z":8, "0":8, "1":8, "2":8, "3":8, "4":8, "5":8, "6":8, "7":8, "8":8, "9":8, "(":51, " ":'T_PRINT', "\n":'T_PRINT'}],
         			  [{"a":8, "b":8, "c":8, "d":8, "e":8, "f":8, "g":8, "h":8, "i":8, "j":8, "k":8, "l":8, "m":8, "n":8, "o":8, "p":8, "q":8, "r":8, "s":8, "t":8, "u":8, "v":8, "w":8, "x":8, "y":8, "z":8, "0":8, "1":8, "2":8, "3":8, "4":8, "5":8, "6":8, "7":8, "8":8, "9":8, "\"":50, " ":'T_ID', "\n":'T_ID'}],
         			  [{"=":10, " ": 'T_ASSIGN'}],
         			  [{" ": 'T_EQUAL'}],
@@ -80,9 +83,10 @@
 					  [{"a":8, "b":8, "c":8, "d":8, "e":8, "f":8, "g":8, "h":8, "i":8, "j":8, "k":8, "l":8, "m":8, "n":8, "o":8, "p":8, "q":8, "r":8, "s":8, "t":8, "u":8, "v":8, "w":8, "x":8, "y":8, "z":8, "0":8, "1":8, "2":8, "3":8, "4":8, "5":8, "6":8, "7":8, "8":8, "9":8, " ":'T_TRUE', "\n":'T_TRUE'}],
 					  [{" ":'T_PLUS', "\n":'T_PLUS'}],
 					  [{" ":'T_EOF', "\n":'T_EOF', "":'T_EOF'}],
-					  [{" ":'T_STRINGEXP', "\n":'T_STRINGEXP'}]
+					  [{" ":'T_STRINGEXP', "\n":'T_STRINGEXP'}],
+					  [{" ": 'T_PRINT'}]
         			 ];
-                 
+         //T_StringEXP === line 50        
         var token = "";
         var j = 0;
         
@@ -92,6 +96,8 @@
         	//console.log("i = " + i);
         	console.log("j = " + j);
         	if (element != " " && element != "\n") {
+        		//Set whiteSpaceCount back to 0
+        		whiteSpaceCount = 0;
         		console.log("Element to check alphabet for " + element);
         		document.getElementById("taOutput").value += "\n\tChecking alphabet for " + element;
         	}
@@ -116,72 +122,96 @@
         		}
         		//Handles unrecognized symbols
         		else {
-        			console.log("Syntax error on line " + lineNumber);
-        			document.getElementById("taOutput").value += "\n\tSyntax error on line " + lineNumber;
-        			
-        			i = code.length + 1; //Stops loop
+        			unrecognizedSymbol = true;
+					errors();
         		}
 	
         	}
         	//When whitespace or newline is encountered
         	else {
-        		//Set the token type (T_ID, T_PRINT, etc.)
-				var type = matrix[j][0][element];
-				console.log("Type " + type);
+        		whiteSpaceCount ++;
+        		
+        		//If the whitespace count is higher than one, disregard them
+        		if (whiteSpaceCount === 1) {
+        			//Set the token type (T_ID, T_PRINT, etc.)
+        			//console.log("Element " + j);
+					var type = matrix[j][0][element];
+					console.log("Type " + type);
 				
-				//Check if quotation is an open quote or a closing quote using a global boolean variable: openQuote
-				if (type == "T_QUOTE" && openQuote) {
-					type = "T_CLOSEQUOTE";
-					openQuote = false;
-				}
-				else if (type == "T_QUOTE" && !openQuote) {
-					type = "T_OPENQUOTE";
-					openQuote = true;
-				}
+					//Check if quotation is an open quote or a closing quote using a global boolean variable: openQuote
+					if (type == "T_QUOTE" && openQuote) {
+						type = "T_CLOSEQUOTE";
+						openQuote = false;
+					}
+					else if (type == "T_QUOTE" && !openQuote) {
+						type = "T_OPENQUOTE";
+						openQuote = true;
+					}
+					
+					//Keeps track of open and closed parenthesis
+					if (type == "T_OPENPAREN" && !openParen) {
+						openParen = true;
+					}
+					else if (type == "T_CLOSEPAREN" && openParen) {
+						openParen = false;
+					}
 				
-				//Check if there is ! before = sign, if there is set type to T_NOTEQUAL
-				if (type == "T_EQUAL" && token.length > 1) {
-					type = "T_NOTEQUAL";
-				}
+					//Check if there is ! before = sign, if there is set type to T_NOTEQUAL
+					if (type == "T_EQUAL" && token.length > 1) {
+						type = "T_NOTEQUAL";
+					}
+					
+					if (type == "T_PRINT" && token === "print(") {
+						openParen = true;
+						var splitToken = token.split("(");
+						newToken(type, splitToken[0]);
+						newToken("T_OPENPAREN", "(");
+					}
 				
-				//Check for EOF
-				if (element === "$") {
-					token = element;
-					type = "T_EOF";
-					endOfFileReached = true;	
-				}
-				
-				//Throw warning if there is anything after $
-				if (endOfFileReached) {
-					i = code.length + 1; //Stops loop
-					warnings();
-				}
-				
-				if (i === code.length-1 && !endOfFileReached) {
-					warnings();
-				}
-				
-				//If newline, increment line count
-				if (element === "\n") {
-					lineNumber++;
-				}
+					//Check for EOF
+					if (element === "$") {
+						token = element;
+						type = "T_EOF";
+						endOfFileReached = true;	
+					}
 						
-        		console.log("Here is your token and type: <" + type + " , " + token + ">");
-        		document.getElementById("taOutput").value += "\n\n\tToken created: <" + type + " , " + token + ">\n";
+        			//console.log("Here is your token and type: <" + type + " , " + token + ">");
+        			//document.getElementById("taOutput").value += "\n\n\tToken created: <" + type + " , " + token + ">\n";
         		
-        		//Type check prevents extra spaces from becoming undefined tokens
-        		if (type != undefined) {
-        			//TODO:push to token array
-        			newToken(type, token);
-        			//TODO:increment token count
-        			tokenCount++;
+        			//Type check prevents extra spaces from becoming undefined tokens
+        			if (type != undefined) {
+        				//TODO:push to token array
+        				newToken(type, token);
+        				//TODO:increment token count
+        				tokenCount++;
+        			}
+        			
+					//Throw warning if there is anything after $
+					if (endOfFileReached) {
+						i = code.length + 1; //Stops loop
+						warnings();
+					}
+				
+					//Throw a warning if EOF was reached without the use of a $
+					if (i === code.length-1 && !endOfFileReached) {
+						warnings();
+					}
+					
+					//If newline, increment line count
+					if (element === "\n") {
+						lineNumber++;
+					}
+        		
+        			token = ""; //Clear out previous token
+  	    			j = 0; //reset the DFA to state zero       			
+        			
         		}
-        		
-        		token = ""; //Clear out previous token
-  	    		j = 0; //reset the DFA to state zero  	    		
+        		//No else, if whiteSpaceCount > 1 just keep moving through the source code until a non whitespace element is reached   	    		
         		
         	}
-        }  
+        } 
+        //Check for open strings and expressions
+        errors(); 
         document.getElementById("taOutput").value += "\n\nLexing Complete";
         
         console.log(tokenArray); //Prints out token array
@@ -192,6 +222,8 @@ function newToken(type, token) {
 	var newToken = "<" + type + " , " + token + ">";
 	
 	tokenArray.push(newToken);
+    console.log("Here is your token and type: <" + type + " , " + token + ">");
+    document.getElementById("taOutput").value += "\n\n\tToken created: <" + type + " , " + token + ">\n";
 	
 }
     
@@ -213,4 +245,24 @@ function warnings() {
 		code2 += "\n\n$";
 		document.getElementById("taSourceCode").value = code2;
 	}	
+}
+
+function errors() {
+	//Syntax Error
+	if (unrecognizedSymbol) {
+		console.log("ERROR: Syntax error on line " + lineNumber);
+        document.getElementById("taOutput").value += "\n\tSyntax error on line " + lineNumber;
+        			
+        i = code.length + 1; //Stops loop
+	}
+	
+	if (openQuote) {
+		console.log("Unended String");
+        document.getElementById("taOutput").value += "\n\tERROR: Unended String";		
+	}
+	
+	if (openParen) {
+		console.log("Unended expression");
+        document.getElementById("taOutput").value += "\n\tERROR: Unended expression";		
+	}
 }
