e9a0bc3ab2f1b8e4143c7c737b0dbc40a9d92f1c
Mike Hunter <randomemail149@gmail.com>
2014-03-13 16:42:51 -0400

    Added parse functions

--- parse.js
+++ parse.js
@@ -1,1 +1,89 @@
 /* parse.js */
+
+function parseProgram() {
+	
+}
+
+function parseBlock() {
+	
+}
+
+function parseStatementList() {
+	
+}
+
+function parseStatement() {
+	
+}
+
+function parsePrintStatement() {
+	
+}
+
+function parseAssignmentStatement() {
+	
+}
+
+function parseVarDecl() {
+	
+}
+
+function parseWhileStatement() {
+	
+}
+
+function parseIfStatement() {
+	
+}
+
+function parseExpr() {
+	
+}
+
+function parseIntExpr() {
+	
+}
+
+function parseStringExpr() {
+	
+}
+
+function parseBooleanExpr() {
+	
+}
+
+function parseId() {
+	
+}
+
+function parseCharList() {
+	
+}
+
+function parseType() {
+	
+}
+
+function parseChar() {
+	
+}
+
+function parseSpace() {
+	
+}
+
+function parseDigit() {
+	
+}
+
+function parseBoolop() {
+	
+}
+
+function parseBoolval() {
+	
+}
+
+function parseIntop() {
+	
+}
